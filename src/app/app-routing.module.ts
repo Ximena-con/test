e6c6418fc1fc4261ed8e95d687c4f77f733a2a34
xin.wang02 <xin.wang02@united-imaging.com>
2021-06-04 16:17:12 +0800

    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HolidayComponent } from './holiday/holiday.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'holiday', component: HolidayComponent },
+  { path: '**', component: HolidayComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
